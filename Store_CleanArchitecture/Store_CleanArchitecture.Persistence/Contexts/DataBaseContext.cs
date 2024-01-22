7dd7beec2e90f55ffc626108a0026a461e99eedd
Sara Brm <sarabayrami01@gmail.com>
2024-01-22 21:58:56 +0330

    store - user structure is implemented

---
+++ Store_CleanArchitecture/Store_CleanArchitecture.Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store_CleanArchitecture.Application.Interfaces.Contexts;
+using Store_CleanArchitecture.Domain.Entities.Users;
+
+namespace Store_CleanArchitecture.Persistence.Contexts
+{
+    public class DataBaseContext : DbContext, IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+
+    }
+}
